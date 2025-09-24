2974e48ca6b78c9df21f8c45a1f052ee129d20e1
Nicolas Desmares <nicolasdesmares@MacBook-Pro-de-Nicolas.local>
2025-09-24 12:08:03 +0200

    last commit

--- cypress/e2e/smoke/default-loadout.cy.ts
+++ cypress/e2e/smoke/default-loadout.cy.ts
@@ -1,40 +1,35 @@
 describe("Create a user and check default loadout", () => {
-const username = `user${Date.now()}`;
+  const username = `user${Date.now()}`;
   const email = `user${Date.now()}@example.com`;
   const password = "Password123!";
   before(() => {
 	cy.signup(username, email, password);
-	cy.visit("fr-FR/armory/weapons");
   });
 
-  it("MX42 should be default rifle", () => {
+  it.only("MX42 should be default rifle", () => {
+	cy.visit("fr-FR/armory/weapons");
 	cy.origin("https://app-qtesting.eva.gg", () => {
-        cy.contains('div', 'MX42')
-            .children('span')
-            .then((text) => {
-                expect(text).to.equal('principale');
-            });
-        });
-  });
+		cy.get('a[href="/fr-FR/armory/weapons/Mx42"]').click();
+		cy.url().should('include', `/fr-FR/armory/weapons/Mx42`);
+		cy.get('button').contains('Principale').should("be.disabled")
+		cy.get('span').contains('Rookie');
+		});
+	});
 
   it("T1 Gauss should be default sidearm", () => {
 	cy.origin("https://app-qtesting.eva.gg", () => {
-        cy.contains('div', 'T1 Gauss')
-            .children('span')
-            .then((text) => {
-                expect(text).to.equal('principale');
-            });
-        });
+        cy.get('a[href="/fr-FR/armory/weapons/T1Gauss"]').click();
+		cy.get('button').contains('Principale').should("be.disabled")
+		cy.get('span').contains('Rookie');
   });
+});
 
   it("Trooper should be default skin", () => {
 	cy.visit("fr-FR/armory/characters");
 	cy.origin("https://app-qtesting.eva.gg", () => {
-		cy.contains('div', 'Trooper')
-			.children('span')
-			.then((text) => {
-				expect(text).to.equal('principale');
-			});
-		});	
-});
+		cy.get('a[href="/fr-FR/armory/characters/trooper"]').click();
+		cy.get('button').contains('Principale').should("be.disabled")
+		cy.get('span').contains('Rookie');
+		});
+   });
 });
